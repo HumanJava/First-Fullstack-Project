c7c96cca31d1366c5a8ac76876ed7831ebeda426
Hyun <therunningllama@gmail.com>
2025-08-25 21:33:49 +0100

    added frontend framework with react and chakra ui

---
+++ frontend/src/main.jsx
@@ -0,0 +1,30 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import { Provider } from "@/components/ui/provider"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+
+
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode >,
+)
+
+
