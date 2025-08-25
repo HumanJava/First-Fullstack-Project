5f0bc32b6133540d7387ba825758d0bd1ba4e464
Hyun <therunningllama@gmail.com>
2025-08-25 17:51:54 +0100

    refactors into routes and controllers

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.js';
+import productRoutes from './routes/product.js';
 
 dotenv.config();
 
@@ -10,51 +10,7 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON req.bodies
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products });
-    } catch (error) {
-        console.error(`❌ Error in Fetch products : ${error.message}`);
-        res.status(500).json({ success: false, message: error.message });
-    }
-});
-
-
-app.post("/api/products", async (req, res) => {
-    const product = req.body; // users will send data
-
-    if (!product.name || !product.price || !product.image) {
-
-        return res.status(400).json({ success: false, message: "Please fill out all values" });
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error(`❌ Error in Create product : ${error.message}`);
-        res.status(500).json({ success: false, message: error.message });
-    }
-
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log("id: ", id);
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted successfully" });
-    }
-    catch (error) {
-        res.status(404).json({ success: false, message: "Product not found" });
-
-    }
-});
-
+app.use("/api/products", productRoutes)
 
 console.log(process.env.MONGO_URI);
 
