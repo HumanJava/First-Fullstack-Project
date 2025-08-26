092aa883b474433ccdef5e470c876226175e6ca9
Hyun <therunningllama@gmail.com>
2025-08-26 02:18:46 +0100

    added frontend and working on Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Container, Flex, HStack, Text, Button, Fade } from "@chakra-ui/react";
+import { FaPlusMinus } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { useColorMode } from "@chakra-ui/react";
+import { FaLightbulb, FaRegLightbulb } from "react-icons/fa";
+
+
+
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        direction={{
+          base: "column",
+          sm: "row"
+        }}>
+
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to="/">Product Store 🛒</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems="center">
+          <Button as={Link} to="/create" leftIcon={<FaPlusMinus fontSize={20} />}>
+            New
+          </Button>
+          <Button onClick={toggleColorMode}>
+            <Fade in={colorMode === "light"}>
+              {colorMode === "light" ? <FaRegLightbulb /> : <FaLightbulb />}
+            </Fade>
+          </Button>
+        </HStack>
+      </Flex>
+    </Container >
+
+  )
+}
+
+export default Navbar
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,30 +1,15 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-import { Provider } from "@/components/ui/provider"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider>
-
-
-        <App />
-      </Provider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode >,
-)
-
-
+  </React.StrictMode>
+);
