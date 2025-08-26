c1fe0e1a4b12b5ad5b86f238dcdf8e7df7c1b06f
Hyun <therunningllama@gmail.com>
2025-08-26 02:23:36 +0100

    updated README.MD

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 5 }} */}
-                style={{ display: "inline-flex", alignItems: "center" }}
+                transition={{ duration: 5 }}
+                style={{ display: "inline-flex", alignItems: "center" }} */}
               >
                 {colorMode === "light" ? <FaRegLightbulb /> : <FaLightbulb />}
               {/* </motion.span>
